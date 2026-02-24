f2fd27a75627f9520f5e45ce5f2029b0355810b8
Joao-ALD <joaovictorald@gmail.com>
2026-02-24 15:03:12 -0300

    err fix

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+// Importação necessária para o Android funcionar
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
+// CORREÇÃO 1: Criar o Drawer fora da função App
+const Drawer = createDrawerNavigator();
 
 function HomeScreen() {
   return (
@@ -15,14 +18,19 @@ function HomeScreen() {
 }
 
 export default function App() {
-  const Drawer = createDrawerNavigator();
   return (
-    <NavigationContainer>
-      <Drawer.Navigator>
-        {/* Aqui definimos o nome da rota e qual componente ela vai abrir */}
-        <Drawer.Screen name="Home" component={HomeScreen} />
-      </Drawer.Navigator>
-    </NavigationContainer>
+    // CORREÇÃO 2: Envolver tudo com o GestureHandlerRootView
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <Drawer.Navigator>
+          <Drawer.Screen 
+            name="Home" 
+            component={HomeScreen} 
+            options={{ title: 'Início' }}
+          />
+        </Drawer.Navigator>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
 
